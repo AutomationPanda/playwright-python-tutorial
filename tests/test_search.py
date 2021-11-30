391c9605eaec55d47d4de09a4e0fc0673586cb97
Pandy Knight <andylpk.247@gmail.com>
2021-11-30 13:19:27 -0500

    Implemented raw Playwright calls in test

--- tests/test_search.py
+++ tests/test_search.py
@@ -3,9 +3,15 @@ These tests cover DuckDuckGo searches.
 """
 
 
-def test_basic_duckduckgo_search():
+def test_basic_duckduckgo_search(page):
+
     # Given the DuckDuckGo home page is displayed
+    page.goto('https://www.duckduckgo.com')
+
     # When the user searches for a phrase
+    page.fill('#search_form_input_homepage', 'panda')
+    page.click('#search_button_homepage')
+
     # Then the search result query is the phrase
     # And the search result links pertain to the phrase
     # And the search result title contains the phrase
