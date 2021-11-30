36657fa23b305dffbb87d1ca9fc17ee30a9709d3
Pandy Knight <andylpk.247@gmail.com>
2021-11-30 15:25:26 -0500

    Added Playwright calls for assertions

--- tests/test_search.py
+++ tests/test_search.py
@@ -13,6 +13,13 @@ def test_basic_duckduckgo_search(page):
     page.click('#search_button_homepage')
 
     # Then the search result query is the phrase
+    assert 'panda' == page.input_value('#search_form_input')
+
     # And the search result links pertain to the phrase
+    page.locator('.result__title a.result__a >> nth=4').wait_for()
+    titles = page.locator('.result__title a.result__a').all_text_contents()
+    matches = [t for t in titles if 'panda' in t.lower()]
+    assert len(matches) > 0
+
     # And the search result title contains the phrase
-    pass
+    assert 'panda' in page.title()
