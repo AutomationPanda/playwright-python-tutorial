6a7c2340149238036b9c8f05251f371c83d2040f
Pandy Knight <andylpk.247@gmail.com>
2022-03-08 17:05:31 -0500

    Restructured API test code

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,17 @@
 This module contains shared fixtures.
 """
 
+# ------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------
+
+import os
 import pytest
+
+
+# ------------------------------------------------------------
+# DuckDuckGo search fixtures
+# ------------------------------------------------------------
 
 from pages.result import DuckDuckGoResultPage
 from pages.search import DuckDuckGoSearchPage
@@ -16,3 +26,82 @@ def result_page(page):
 @pytest.fixture
 def search_page(page):
     return DuckDuckGoSearchPage(page)
+
+
+# ------------------------------------------------------------
+# GitHub project fixtures
+# ------------------------------------------------------------
+
+# Environment variables
+
+def _get_env_var(varname):
+    value = os.getenv(varname)
+    assert value, f'{varname} is not set'
+    return value
+
+
+@pytest.fixture(scope='session')
+def gh_username():
+    return _get_env_var('GITHUB_USERNAME')
+
+
+@pytest.fixture(scope='session')
+def gh_password():
+    return _get_env_var('GITHUB_PASSWORD')
+
+
+@pytest.fixture(scope='session')
+def gh_access_token():
+    return _get_env_var('GITHUB_ACCESS_TOKEN')
+
+
+@pytest.fixture(scope='session')
+def gh_project_name():
+    return _get_env_var('GITHUB_PROJECT_NAME')
+
+
+# Request context
+
+@pytest.fixture(scope='session')
+def gh_context(playwright, gh_access_token):
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {gh_access_token}"}
+
+    request_context = playwright.request.new_context(
+        base_url="https://api.github.com",
+        extra_http_headers=headers)
+
+    yield request_context
+    request_context.dispose()
+
+
+# GitHub project requests
+
+@pytest.fixture(scope='session')
+def gh_project(gh_context, gh_username, gh_project_name):
+    resource = f'/users/{gh_username}/projects'
+    response = gh_context.get(resource)
+    assert response.ok
+    
+    name_match = lambda x: x['name'] == gh_project_name
+    filtered = filter(name_match, response.json())
+    project = list(filtered)[0]
+    assert project
+
+    return project
+
+
+@pytest.fixture()
+def project_columns(gh_context, gh_project):
+    response = gh_context.get(gh_project['columns_url'])
+    assert response.ok
+
+    columns = response.json()
+    assert len(columns) >= 2
+    return columns
+
+
+@pytest.fixture()
+def project_column_ids(project_columns):
+    return list(map(lambda x: x['id'], project_columns))
--- tests/test_github_project.py
+++ tests/test_github_project.py
@@ -1,108 +1,58 @@
 """
-These tests cover API tests for GitHub projects.
+These tests cover API interactions for GitHub projects.
 """
 
-import os
-import pytest
+# ------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------
+
 import time
 
 from playwright.sync_api import expect
 
 
-def _get_env_var(varname):
-    value = os.getenv(varname)
-    assert value, f'{varname} is not set'
-    return value
+# ------------------------------------------------------------
+# A pure API test
+# ------------------------------------------------------------
 
+def test_create_project_card(gh_context, project_column_ids):
 
-@pytest.fixture(scope='session')
-def gh_username():
-    return _get_env_var('GITHUB_USERNAME')
-
-
-@pytest.fixture(scope='session')
-def gh_password():
-    return _get_env_var('GITHUB_PASSWORD')
-
-
-@pytest.fixture(scope='session')
-def gh_access_token():
-    return _get_env_var('GITHUB_ACCESS_TOKEN')
-
-
-@pytest.fixture(scope='session')
-def gh_project_name():
-    return _get_env_var('GITHUB_PROJECT_NAME')
-
-
-@pytest.fixture(scope="session")
-def gh_request_context(playwright, gh_access_token):
-    headers = {
-        "Accept": "application/vnd.github.v3+json",
-        "Authorization": f"token {gh_access_token}"}
-
-    request_context = playwright.request.new_context(
-        base_url="https://api.github.com",
-        extra_http_headers=headers)
-
-    yield request_context
-    request_context.dispose()
-
-
-@pytest.fixture()
-def gh_project(gh_request_context, gh_username, gh_project_name):
-    resource = f'/users/{gh_username}/projects'
-    response = gh_request_context.get(resource)
-    assert response.ok
-    
-    name_match = lambda x: x['name'] == gh_project_name
-    filtered = filter(name_match, response.json())
-    project = list(filtered)[0]
-    assert project
-
-    return project
-
-
-@pytest.fixture()
-def project_columns(gh_request_context, gh_project):
-    response = gh_request_context.get(gh_project['columns_url'])
-    assert response.ok
-
-    columns = response.json()
-    assert len(columns) >= 2
-    return columns
-
-
-@pytest.fixture()
-def project_column_ids(project_columns):
-    return list(map(lambda x: x['id'], project_columns))
-
-
-def test_create_project_card(gh_request_context, project_column_ids):
+    # Prep test data
     now = time.time()
     note = f'A new task at {now}!'
 
-    c_response = gh_request_context.post(
+    # Create a new card
+    c_response = gh_context.post(
         f'/projects/columns/{project_column_ids[0]}/cards',
         data={'note': note})
     assert c_response.ok
 
+    # Retrieve the newly created card
     card_id = c_response.json()['id']
-    r_response = gh_request_context.get(f'/projects/columns/cards/{card_id}')
+    r_response = gh_context.get(f'/projects/columns/cards/{card_id}')
     assert r_response.ok
     assert r_response.json()['note'] == note
 
 
-def test_move_project_card(gh_request_context, gh_project, project_column_ids, page, gh_username, gh_password):
-    # Prep a card via API
+# ------------------------------------------------------------
+# A hybrid UI/API test
+# ------------------------------------------------------------
+
+def test_move_project_card(gh_context, gh_project, project_column_ids, page, gh_username, gh_password):
+
+    # Prep test data
+    col0 = project_column_ids[0]
+    col1 = project_column_ids[1]
     now = time.time()
     note = f'Move this card at {now}'
-    c_response = gh_request_context.post(
-        f'/projects/columns/{project_column_ids[0]}/cards',
+
+    # Create a new card via API
+    c_response = gh_context.post(
+        f'/projects/columns/{col0}/cards',
         data={'note': note})
     assert c_response.ok
 
-    # Login
+    # Log in via UI
     page.goto(f'https://github.com/login')
     page.fill('id=login_field', gh_username)
     page.fill('id=password', gh_password)
@@ -113,12 +63,14 @@ def test_move_project_card(gh_request_context, gh_project, project_column_ids, p
     page.goto(f'https://github.com/users/{gh_username}/projects/{number}')
 
     # Move a card via web UI
-    page.drag_and_drop(f'text="{note}"', f'id=column-cards-{project_column_ids[1]}')
+    page.drag_and_drop(f'text="{note}"', f'id=column-cards-{col1}')
 
-    # Verify the card is in the new column
-    expect(page.locator(f'//div[@id="column-cards-{project_column_ids[1]}"]//p[contains(text(), "{note}")]')).to_be_visible()
+    # Verify the card is in the new column via UI
+    card_xpath = f'//div[@id="column-cards-{col1}"]//p[contains(text(), "{note}")]'
+    expect(page.locator(card_xpath)).to_be_visible()
 
-    # Verify the backend is updated
-    r_response = gh_request_context.get(f'/projects/columns/cards/{c_response.json()["id"]}')
+    # Verify the backend is updated via API
+    card_id = c_response.json()['id']
+    r_response = gh_context.get(f'/projects/columns/cards/{card_id}')
     assert r_response.ok
-    assert r_response.json()['column_url'].endswith(str(project_column_ids[1]))
+    assert r_response.json()['column_url'].endswith(str(col1))
