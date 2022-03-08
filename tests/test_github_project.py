47e45b9d88ca168131f667683d08672881e7d686
Pandy Knight <andylpk.247@gmail.com>
2022-03-08 16:46:04 -0500

    github_ -> gh_

--- tests/test_github_project.py
+++ tests/test_github_project.py
@@ -16,30 +16,30 @@ def _get_env_var(varname):
 
 
 @pytest.fixture(scope='session')
-def github_username():
+def gh_username():
     return _get_env_var('GITHUB_USERNAME')
 
 
 @pytest.fixture(scope='session')
-def github_password():
+def gh_password():
     return _get_env_var('GITHUB_PASSWORD')
 
 
 @pytest.fixture(scope='session')
-def github_access_token():
+def gh_access_token():
     return _get_env_var('GITHUB_ACCESS_TOKEN')
 
 
 @pytest.fixture(scope='session')
-def github_project_name():
+def gh_project_name():
     return _get_env_var('GITHUB_PROJECT_NAME')
 
 
 @pytest.fixture(scope="session")
-def github_request_context(playwright, github_access_token):
+def gh_request_context(playwright, gh_access_token):
     headers = {
         "Accept": "application/vnd.github.v3+json",
-        "Authorization": f"token {github_access_token}"}
+        "Authorization": f"token {gh_access_token}"}
 
     request_context = playwright.request.new_context(
         base_url="https://api.github.com",
@@ -50,12 +50,12 @@ def github_request_context(playwright, github_access_token):
 
 
 @pytest.fixture()
-def github_project(github_request_context, github_username, github_project_name):
-    resource = f'/users/{github_username}/projects'
-    response = github_request_context.get(resource)
+def gh_project(gh_request_context, gh_username, gh_project_name):
+    resource = f'/users/{gh_username}/projects'
+    response = gh_request_context.get(resource)
     assert response.ok
     
-    name_match = lambda x: x['name'] == github_project_name
+    name_match = lambda x: x['name'] == gh_project_name
     filtered = filter(name_match, response.json())
     project = list(filtered)[0]
     assert project
@@ -64,8 +64,8 @@ def github_project(github_request_context, github_username, github_project_name)
 
 
 @pytest.fixture()
-def project_columns(github_request_context, github_project):
-    response = github_request_context.get(github_project['columns_url'])
+def project_columns(gh_request_context, gh_project):
+    response = gh_request_context.get(gh_project['columns_url'])
     assert response.ok
 
     columns = response.json()
@@ -78,39 +78,39 @@ def project_column_ids(project_columns):
     return list(map(lambda x: x['id'], project_columns))
 
 
-def test_create_project_card(github_request_context, project_column_ids):
+def test_create_project_card(gh_request_context, project_column_ids):
     now = time.time()
     note = f'A new task at {now}!'
 
-    c_response = github_request_context.post(
+    c_response = gh_request_context.post(
         f'/projects/columns/{project_column_ids[0]}/cards',
         data={'note': note})
     assert c_response.ok
 
     card_id = c_response.json()['id']
-    r_response = github_request_context.get(f'/projects/columns/cards/{card_id}')
+    r_response = gh_request_context.get(f'/projects/columns/cards/{card_id}')
     assert r_response.ok
     assert r_response.json()['note'] == note
 
 
-def test_move_project_card(github_request_context, github_project, project_column_ids, page, github_username, github_password):
+def test_move_project_card(gh_request_context, gh_project, project_column_ids, page, gh_username, gh_password):
     # Prep a card via API
     now = time.time()
     note = f'Move this card at {now}'
-    c_response = github_request_context.post(
+    c_response = gh_request_context.post(
         f'/projects/columns/{project_column_ids[0]}/cards',
         data={'note': note})
     assert c_response.ok
 
     # Login
     page.goto(f'https://github.com/login')
-    page.fill('id=login_field', github_username)
-    page.fill('id=password', github_password)
+    page.fill('id=login_field', gh_username)
+    page.fill('id=password', gh_password)
     page.click('input[name="commit"]')
 
     # Load the project page
-    number = github_project['number']
-    page.goto(f'https://github.com/users/{github_username}/projects/{number}')
+    number = gh_project['number']
+    page.goto(f'https://github.com/users/{gh_username}/projects/{number}')
 
     # Move a card via web UI
     page.drag_and_drop(f'text="{note}"', f'id=column-cards-{project_column_ids[1]}')
@@ -119,6 +119,6 @@ def test_move_project_card(github_request_context, github_project, project_colum
     expect(page.locator(f'//div[@id="column-cards-{project_column_ids[1]}"]//p[contains(text(), "{note}")]')).to_be_visible()
 
     # Verify the backend is updated
-    r_response = github_request_context.get(f'/projects/columns/cards/{c_response.json()["id"]}')
+    r_response = gh_request_context.get(f'/projects/columns/cards/{c_response.json()["id"]}')
     assert r_response.ok
     assert r_response.json()['column_url'].endswith(str(project_column_ids[1]))
