89f8256ad025eee898bff204b0d72ed98176d421
Pandy Knight <andylpk.247@gmail.com>
2022-03-11 14:58:40 -0500

    Added type hints and Playwright improvements to code

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,10 +2,15 @@
 These tests cover DuckDuckGo searches.
 """
 
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
 from playwright.sync_api import expect, Page
 
 
-def test_basic_duckduckgo_search(search_page, result_page) -> None:
+def test_basic_duckduckgo_search(
+    page: Page,
+    search_page: DuckDuckGoSearchPage,
+    result_page: DuckDuckGoResultPage) -> None:
     
     # Given the DuckDuckGo home page is displayed
     search_page.load()
@@ -14,11 +19,11 @@ def test_basic_duckduckgo_search(search_page, result_page) -> None:
     search_page.search('panda')
 
     # Then the search result query is the phrase
-    assert 'panda' == result_page.search_input_value()
+    expect(result_page.search_input).to_have_value('panda')
 
     # And the search result links pertain to the phrase
     assert result_page.result_link_titles_contain_phrase('panda')
 
     # And the search result title contains the phrase
-    assert 'panda' in result_page.title()
+    expect(page).to_have_title('panda at DuckDuckGo')
 
